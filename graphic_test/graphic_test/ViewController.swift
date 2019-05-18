fb2fc5de382fa1803997626c44151f0653111075
skomatin <skomatin@ucsd.edu>
2019-05-18 12:11:04 -0700

    free rotation on shoe working

--- graphic_test/graphic_test/ViewController.swift
+++ graphic_test/graphic_test/ViewController.swift
@@ -9,39 +9,26 @@
 import UIKit
 import SceneKit
 
-
 class ViewController: UIViewController {
 
+    private var shoeNode : SCNNode?
+    
     @IBOutlet weak var sceneView: SCNView!
     
     @IBAction func undoButton(_ sender: Any) {
 
     }
+    
     @IBAction func rotateButton(_ sender: Any) {
-       let rotateOne = SCNAction.rotateBy(x: 1, y: 0, z: 1, duration: 0.5)
-        sceneView.scene?.rootNode.runAction(rotateOne)
+        sceneView.scene?.rootNode.isPaused = false
+       let rotateOne = SCNAction.rotateBy(x: 1, y: 0, z: 0, duration: 0.5)
+        shoeNode?.runAction(rotateOne)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //setup()
         // Do any additional setup after loading the view.
-        let action = SCNAction.moveBy(x: 0, y: 2, z: 0, duration: 1)
-        sceneView.scene?.rootNode.runAction(action)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        let action = SCNAction.moveBy(x: 0, y: -20, z: 0, duration: 1)
-        sceneView.scene?.rootNode.runAction(action)
-    }
-
-    func setup() {
-        //        let sceneView = SCNView(frame: self.view.frame)
-        //        self.view.addSubview(sceneView)
-        //
-        //        let scene = SCNScene(named: "shoe.scn")
-        //        sceneView.scene = scene
+        shoeNode = sceneView.scene?.rootNode.childNode(withName: "shoe", recursively: false)
     }
 
 }
