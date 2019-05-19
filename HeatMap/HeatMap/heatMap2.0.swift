59d5da334bd0a3418a2492520adf141faf998ed2
skomatin <skomatin@ucsd.edu>
2019-05-18 21:36:42 -0700

    getting the sole image pixel array

--- HeatMap/HeatMap/heatMap2.0.swift
+++ HeatMap/HeatMap/heatMap2.0.swift
@@ -21,81 +21,94 @@ class HeatMap_2_0: UIViewController{
     
     @IBOutlet weak var right_sole_icon: UIImageView!
     
+    let num_Rows = 14
+    let num_Cols = 5
+    
     var pixelMatrix = [[PixelData]]()
+    var indexMatrix = [[Int]]() //contains the x and y location for all 70 pressure points
+    var pressure_Data = [[CGFloat]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let width = self.view.bounds.width
-//        let height = self.view.bounds.height
-//
-//        let image_width: CGFloat = 50
-//        let image_height: CGFloat = 140
-//
-//        var pixels = [PixelData]()
-//
-//        let red = PixelData(a: 255, r: 255, g: 0, b: 0)
-//        let green = PixelData(a: 255, r: 0, g: 255, b: 0)
-//        let blue = PixelData(a: 255, r: 0, g: 0, b: 255)
-//
-//        for _ in 1...2333 {
-//            pixels.append(red)
-//        }
-//        for _ in 1...2333 {
-//            pixels.append(green)
-//        }
-//        for _ in 1...2334 {
-//            pixels.append(blue)
-//        }
-//
-//        let image = imageFromARGB32Bitmap(pixels: pixels, width: Int(image_width), height: Int(image_height))
-//        let imageView = UIImageView(frame: CGRect(x: width * 0.5 - image_width * 0.5, y: height * 0.5 - image_height * 0.5, width: image_width, height: image_height))
-//        imageView.image = image
-        //        self.view.addSubview(imageView)
 
+        let soleImage: UIImage = UIImage(named: "right_sole_icon")!
         
-        let pixelData = right_sole_icon.image?.pixelData()
-        let image_width = right_sole_icon.image!.size.width             // 82
-        let image_height = right_sole_icon.image!.size.height          // 199
+        let pixelData = soleImage.pixelData()
+        let image_width = Int(soleImage.size.width)            // 82
+        let image_height = Int(soleImage.size.height)          // 199
         
-        for j in 0..<Int(image_height){
+        /*
+        * fill in the pixelMatrix with the values from the given shoe
+        */
+        for j in 0..<image_height{
             var pixArray = [PixelData]()
-            for i in 0..<Int(image_width){
-                if let pix = pixelData![j*Int(image_height) + i]{
-                
+            for i in 0..<image_width{
+                if let pix = pixelData?[j*Int(image_width) + i]{
+                    if pix.r == 0 && pix.g == 0 && pix.b == 0 {
+                        pixArray.append(pix)
+                    }
+                    else {
+                        pixArray.append(PixelData(a: 255, r: 255, g: 255, b: 255))
+                    }
                 }
-                pixArray.append(PixelData(a: 255, r: 255, g: 255, b: 255))
+                else {
+                    fatalError("pix cannot be found")
+                }
             }
             pixelMatrix.append(pixArray)
         }
         
-        for i in pixelData! {
-            if i.r != 0 && i.b != 0 && i.g != 0{
-                print(i)
-            }
-        }
-        print(pixelData?.count)
-        print(image_width)
-        print(image_height)
+        right_sole_icon.image = imageFromARGB32Bitmap(pixels: pixelMatrix, width: image_width, height: image_height)
+        
+//        print(pixelMatrix.flatMap{$0}.count)
+//        print(image_width)
+//        print(image_height)
         print("done!!")
     }
+    
+    func update_index() {
+        //update indexMatrix
+        
+    }
+    
+    func updateMap() {
+        for j in 0..<(num_Cols*num_Rows) {
+            var point = indexMatrix[j]
+            if var pix = pixelMatrix[point[0]][point[1]] as PixelData? {
+                pix = PixelData(a: 255, r: 255, g: 0, b: 0) // update this according to recieved pressure data
+                pixelMatrix[point[0]][point[1]] = pix
+            }
+        }
+    }
+    
 }
 
 extension HeatMap_2_0{
-    func imageFromARGB32Bitmap(pixels: [PixelData], width: Int, height: Int) -> UIImage? {
+    func imageFromARGB32Bitmap(pixels: [[PixelData]], width: Int, height: Int) -> UIImage? {
         guard width > 0 && height > 0 else { return nil }
-        guard pixels.count == width * height else { return nil }
+        guard (pixels.flatMap{$0}.count) == width * height else { return nil }
         
         let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
         let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)
         let bitsPerComponent = 8
         let bitsPerPixel = 32
         
-        var data = pixels // Copy to mutable []
+        var temp_pixels = [PixelData]()
+        for j in 0..<height {
+            for i in 0..<width {
+                if let pix = pixels[j][i] as PixelData? {
+                    temp_pixels.append(pix)
+                }
+            }
+        }
+        
+        var data = temp_pixels // Copy to mutable []
         guard let providerRef = CGDataProvider(data: NSData(bytes: &data,
-                                                            length: data.count * MemoryLayout<PixelData>.size)
+                length: data.count * MemoryLayout<PixelData>.size)
             )
-            else { return nil }
+            else {
+                print("provider ref failed")
+                return nil }
         
         guard let cgim = CGImage(
             width: width,
@@ -110,7 +123,9 @@ extension HeatMap_2_0{
             shouldInterpolate: true,
             intent: .defaultIntent
             )
-            else { return nil }
+            else {
+                print("cgim failed")
+                return nil }
         
         print("returning image")
         return UIImage(cgImage: cgim)
@@ -118,30 +133,12 @@ extension HeatMap_2_0{
 }
 
 extension UIImage {
-//    func pixelData() -> [UInt8]? {
-//        let size = self.size
-//        let dataSize = size.width * size.height * 4
-//        var pixelData = [UInt8](repeating: 0, count: Int(dataSize))
-//        let colorSpace = CGColorSpaceCreateDeviceRGB()
-//        let context = CGContext(data: &pixelData,
-//                                width: Int(size.width),
-//                                height: Int(size.height),
-//                                bitsPerComponent: 8,
-//                                bytesPerRow: 4 * Int(size.width),
-//                                space: colorSpace,
-//                                bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue)
-//        guard let cgImage = self.cgImage else { return nil }
-//        context?.draw(cgImage, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-//
-//        return pixelData
-//    }
     func pixelData() -> [PixelData]? {
         let size = self.size
         let dataSize = size.width * size.height * 4
-        var pixelData = [UInt8](repeating: 0, count: Int(dataSize))
+        var pixelData = [UInt8](repeating: 255, count: Int(dataSize))
         var pixelData2 = [PixelData]()
         let colorSpace = CGColorSpaceCreateDeviceRGB()
-//        CGImageAlphaInfo.premultipliedFirst.rawValue
         
         let context = CGContext(data: &pixelData,
                                 width: Int(size.width),
