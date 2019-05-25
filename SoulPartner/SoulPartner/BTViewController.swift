7cbbd084b976bb526a9916f741ae1e0cabcc3b90
skomatin <skomatin@ucsd.edu>
2019-05-25 06:36:57 -0700

    getting there

---
+++ SoulPartner/SoulPartner/BTViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BTViewController.swift
+//  SoulPartner
+//
+//  Created by Saikiran Komatineni on 5/25/19.
+//  Copyright © 2019 Saikiran Komatineni. All rights reserved.
+//
+
+import UIKit
+
+class BTViewController: UIViewController {
+
+    @IBAction func multiPurposeButtonAction(_ sender: Any) {
+    }
+    @IBOutlet weak var multipurposeButton_outlet: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SoulPartner/SoulPartner/ForgotPasswordViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ForgotPasswordViewController.swift
+//  SoulPartner
+//
+//  Created by Saikiran Komatineni on 5/25/19.
+//  Copyright © 2019 Saikiran Komatineni. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    @IBOutlet weak var sendLinkOutlet: UIButton!
+    @IBAction func sendLinkButton(_ sender: Any) {
+    }
+    @IBOutlet weak var emailTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
