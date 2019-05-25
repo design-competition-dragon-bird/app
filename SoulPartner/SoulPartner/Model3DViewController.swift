60ca79005b62ac6af8f2c54287c6da2a5bed5ead
Ravi Patel <rdp2606@gmail.com>
2019-05-25 01:45:35 -0700

    3dmodel added

---
+++ SoulPartner/SoulPartner/Model3DViewController.swift
@@ -0,0 +1,42 @@
+//
+//  Model3DViewController.swift
+//  SoulPartner
+//
+//  Created by Ravi Patel on 5/25/19.
+//  Copyright © 2019 Saikiran Komatineni. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class Model3DViewController: UIViewController {
+
+    private var shoeNode : SCNNode?
+    @IBOutlet weak var sceneView: SCNView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+         shoeNode = sceneView.scene?.rootNode.childNode(withName: "shoe", recursively: false)
+    }
+    
+    @IBAction func rotateButton(_ sender: Any) {
+        sceneView.scene?.rootNode.isPaused = false
+        let rotateOne = SCNAction.rotateBy(x: 1, y: 0, z: 0, duration: 0.5)
+        shoeNode?.runAction(rotateOne)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
