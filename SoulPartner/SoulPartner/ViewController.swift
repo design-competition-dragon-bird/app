19100a3c91eb761095ae1480da202d671bdd507b
skomatin <skomatin@ucsd.edu>
2019-05-25 03:19:04 -0700

    started UI

--- SoulPartner/SoulPartner/ViewController.swift
+++ SoulPartner/SoulPartner/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var forgotButton_outlet: UIButton!
+    @IBOutlet weak var passwordButton_outlet: UIButton!
+    @IBOutlet weak var logInButton_outlet: UIButton!
+    @IBAction func forgotButton_action(_ sender: Any) {
+    }
+    @IBAction func passwordButton_action(_ sender: Any) {
+    }
+    @IBAction func loginButton_action(_ sender: Any) {
+    }
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
